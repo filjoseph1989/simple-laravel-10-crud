487f1199d8fbdfd41c2bb8e58439d533c6d07fe1
fil <filjoseph22@gmail.com>
2023-09-20 23:08:10 +0800

    added store seeder

---
+++ database/seeders/StoreSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class StoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        // Define the number of stores you want to create
+        $numberOfStores = 10;
+
+        // Loop to create stores with sample data
+        for ($i = 1; $i <= $numberOfStores; $i++) {
+            DB::table('stores')->insert([
+                'user_id' => rand(3, 9),
+                'title' => $faker->company,
+                'description' => $faker->paragraph,
+                'image' => 'store_image_' . $i . '.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
