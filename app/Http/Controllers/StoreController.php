37eec1db1080cd6d51a22b16354fca0a291c86ae
fil <filjoseph22@gmail.com>
2023-09-20 22:01:50 +0800

    Added the basic crud for store creation

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -2,9 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Store;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $stores = Store::all();
+        return view('stores.index', compact('stores'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('stores.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+        ]);
+
+        $store = Store::create($validatedData);
+
+        return redirect()->route('stores.index')->with('success', 'Store created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $store = Store::findOrFail($id);
+        return view('stores.show', compact('store'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $store = Store::findOrFail($id);
+        return view('stores.edit', compact('store'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+        ]);
+
+        $store = Store::findOrFail($id);
+        $store->update($validatedData);
+
+        return redirect()->route('stores.index')->with('success', 'Store updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $store = Store::findOrFail($id);
+        $store->delete();
+
+        return redirect()->route('stores.index')->with('success', 'Store deleted successfully.');
+    }
 }
