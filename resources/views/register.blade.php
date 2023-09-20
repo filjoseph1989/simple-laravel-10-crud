255b16c401f655d8f5603b986785b3616f687860
fil <filjoseph22@gmail.com>
2023-09-20 19:52:58 +0800

    styling registration form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,32 +10,33 @@
     ])
 </head>
 <body>
-    <h1>Registration Form</h1>
+    <h1 class="text-base font-bold" style=" font-size: x-large; margin: 40px 50px;">Registration Form</h1>
 
-    <form action="/register" method="post">
-        @csrf
+    <div style="margin: 0 50px;">
+        <form action="/register" method="post">
+            @csrf
 
-        <div class="flex flex-col space-y-4">
-            <div class="flex flex-row items-center">
-                <label for="name" class="mr-2">Name</label>
-                <input type="text" name="name" placeholder="Name" class="w-full border rounded p-2">
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-row items-center">
+                    <label for="name" class="mr-2">Name</label>
+                    <input type="text" name="name" placeholder="Name" class="w-full border rounded p-2">
+                </div>
+                <div class="flex flex-row items-center">
+                    <label for="email" class="mr-2">Email</label>
+                    <input type="email" name="email" placeholder="Email" class="w-full border rounded p-2">
+                </div>
+                <div class="flex flex-row items-center">
+                    <label for="password" class="mr-2">Password</label>
+                    <input type="password" name="password" placeholder="Password" class="w-full border rounded p-2">
+                </div>
+                <div class="flex flex-row items-center">
+                    <label for="password_confirmation" class="mr-2">Confirm Password</label>
+                    <input type="password" name="password_confirmation" placeholder="Confirm Password" class="w-full border rounded p-2">
+                </div>
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Register</button>
             </div>
-            <div class="flex flex-row items-center">
-                <label for="email" class="mr-2">Email</label>
-                <input type="email" name="email" placeholder="Email" class="w-full border rounded p-2">
-            </div>
-            <div class="flex flex-row items-center">
-                <label for="password" class="mr-2">Password</label>
-                <input type="password" name="password" placeholder="Password" class="w-full border rounded p-2">
-            </div>
-            <div class="flex flex-row items-center">
-                <label for="password_confirmation" class="mr-2">Confirm Password</label>
-                <input type="password" name="password_confirmation" placeholder="Confirm Password" class="w-full border rounded p-2">
-            </div>
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Register</button>
-        </div>
-
-   </form>
+        </form>
+    </div>
 </body>
 </html>
 
