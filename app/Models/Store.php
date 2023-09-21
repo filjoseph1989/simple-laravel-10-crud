f29eabc4b72d6c44390d9c2eefbf0bec62bbef06
fil <filjoseph22@gmail.com>
2023-09-21 11:59:56 +0800

    improve stores layout

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     use HasFactory;
+
+    /**
+     * Define a one-to-one relationship with User.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -31,24 +31,48 @@
             <div class="mx-2">
                 <div class="flex columns-3">
                     @foreach ($chunks as $store)
-                        <div class="mx-2 my-4 rounded-md border">
-                            <div class="bg-white rounded-lg shadow-lg">
-                                <div class="flex flex-col p-4">
-                                    <div>
-                                        <img src="{{ $store->image }}" alt="Store image" class="w-full h-32">
-                                    </div>
-                                    <div>
-                                        <h2 class="text-xl font-bold mb-4">
-                                            <a href="http://">{{ $store->title }}</a>
-                                        </h2>
-                                        <i><a href="http://{{ $store->user_id }}">Owner</a> | {{ $store->created_at->format("M d, Y") }}</i>
-                                        <p>{{ implode(' ', array_slice(explode(' ', $store->description), 0, 10)) }}</p>
-                                    </div>
+                        <div class="mx-2 my-4 rounded-md w-1/3 ">
+                            <div class="scale-100 p-6 bg-white dark:bg-gray-900 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                                <div>
+                                    <a href="{{ route('store.show', ['id' => $store->id]) }}" class="flex">
+                                        <div>
+                                            {{-- Todo - Add actual link on the stores table --}}
+                                            @php $store->image = null; @endphp
 
-                                    {{-- TODO - --}}
-                                    <div class="mt-4 flex flex-row">
-                                        <a href="{{ route('store.show', ['id' => $store->id]) }}" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">Visit</a>
-                                        <a href="{{ route('store.edit', ['id' => $store->id]) }}" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">Edit</a>
+                                            @if (is_null($store->image))
+                                                <div class="h-16 w-16 bg-red-50 light:bg-gray-900 flex items-center justify-center rounded-full">
+                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                        class="ml-2 w-7 h-7 stroke-red-500">
+                                                        <path d="M12 2c2.2 0 4 1.8 4 4s-1.8 4-4 4-4-1.8-4-4 1.8-4 4-4zM12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2z"/>
+                                                        <path d="M20 18h-3.3c-.4 0-.7-.2-1.1-.4l-1.6-.8-1.8 3.6c-.3.7-1 1.2-1.8 1.2h-8c-1.1 0-2-.9-2-2v-12c0-1.1.9-2 2-2h12c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2z"/>
+                                                    </svg>
+                                                </div>
+                                            @else
+                                                <div> <img src="{{ $store->image }}" alt="Store image" class="w-full h-32"> </div>
+                                            @endif
+
+                                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">{{ $store->title }}</h2>
+
+                                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">{{ $store->description }}</p>
+                                        </div>
+
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="mt-10">
+                                    {{-- Todo - update the user link --}}
+                                    <i class="text-white text-sm">
+                                        <a href="http://{{ $store->user_id }}">{{ ucfirst($store->user->name) ?? 'Owner\'s Name' }}</a> | Created On
+                                        {{ $store->created_at->format("M d, Y") }}
+                                    </i>
+
+                                    <div class="flex">
+                                        <a href="{{ route('store.edit', ['id' => $store->id]) }}">
+                                            <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded delete-btn mr-1">Edit</button>
+                                        </a>
                                         <form action="{{ route('store.destroy', $store->id) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
